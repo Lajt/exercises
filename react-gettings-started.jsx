a0d9019e5329dc75d288222b5be9261e48029ba5
Lajt <matti815@gmail.com>
2018-04-21 03:36:17 +0200

    react introduction

---
+++ react-gettings-started.jsx
@@ -0,0 +1,47 @@
+// https://app.pluralsight.com/library/courses/react-js-getting-started/table-of-contents
+// repl used: https://jscomplete.com/repl/
+
+
+class App extends React.Component{
+  state = { counter: 0 }
+
+  incrementCounter = (inc) => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + inc
+    }))
+  }
+
+  render() {
+    return(
+      <div>
+        < Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        < Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        < Button incrementValue={10} onClickFunction={this.incrementCounter} />
+        < Button incrementValue={100} onClickFunction={this.incrementCounter} />
+        < Result counter={this.state.counter}/>
+      </div>
+    )
+  }
+}
+
+class Button extends React.Component{
+
+  handleClick = () => {
+    this.props.onClickFunction(this.props.incrementValue);
+  }
+  render(){
+    return(
+      <button onClick={this.handleClick}>
+        +{this.props.incrementValue}
+      </button>
+    )
+  }
+}
+
+const Result = (props) => {
+  return(
+    <div>{props.counter}</div>
+  )
+}
+
+ReactDOM.render(<App />, mountNode);
